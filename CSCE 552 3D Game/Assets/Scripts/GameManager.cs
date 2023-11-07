37bf9d634ec5b3760657cb9c4efd14f47192c420
Benjamin C Goodman <bcgoodman188@gmail.com>
2023-11-07 15:50:07 -0500

    Changed ground color and added UI text for timer, score, wave, and health

--- CSCE 552 3D Game/Assets/Scripts/GameManager.cs
+++ CSCE 552 3D Game/Assets/Scripts/GameManager.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI waveText;
+    public TextMeshProUGUI healthText;
+    public static int score = 0;
+    public static int timerMin = 2;
+    public static int timerSecOne = 0;
+    public static int timerSecTwo = 0;
+    public static int wave = 0;
+    public static int health = 100;
+    void Update() {
+        scoreText.text = "Score: " + score.ToString();
+        timerText.text = "Timer: " + timerMin.ToString() + ":" + timerSecOne.ToString() + "" + timerSecTwo.ToString();
+        waveText.text = "Wave: " + wave.ToString();
+        healthText.text = "Health: " + health.ToString();
+    }
 }
