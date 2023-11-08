4176e366732133a4cf8aea31fde7e50607894e72
Benjamin C Goodman <bcgoodman188@gmail.com>
2023-11-08 16:04:32 -0500

    Added save feature that needs to be corrected

--- CSCE 552 3D Game/Assets/Scripts/PlayerMove.cs
+++ CSCE 552 3D Game/Assets/Scripts/PlayerMove.cs
@@ -7,6 +7,7 @@ public class PlayerMove : MonoBehaviour
     public CharacterController controller;
     public Transform groundCheck;
     public LayerMask groundMask;
+    public int health = 100;
     public float distToGround = 0.3f;
     public float speed = 12f;
     public float gravity = -9.81f;
@@ -18,6 +19,14 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.K)) {
+            Debug.Log("Save ran");
+            SavePlayer();
+        }
+        if(Input.GetKey(KeyCode.L)) {
+            Debug.Log("Load Ran");
+            LoadPlayer();
+        }
         isGrounded = Physics.CheckSphere(groundCheck.position, distToGround, groundMask);
         
         if(isGrounded && velocity.y < 0) {
@@ -38,5 +47,19 @@ public class PlayerMove : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+    public void SavePlayer() {
+        GameSave.SavePlayer(this);
+    }
+    public void LoadPlayer() {
+        PlayerData data = GameSave.LoadPlayer();
+
+        health = data.health;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
     }
 }
