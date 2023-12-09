9516e25cce44d502cf8f3204c2e11eb989d97543
Benjamin C Goodman <bcgoodman188@gmail.com>
2023-12-09 15:17:04 -0500

    finished doubt and rage

--- CSCE 552 3D Game/Assets/Scripts/AngerEnemy.cs
+++ CSCE 552 3D Game/Assets/Scripts/AngerEnemy.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class AngerEnemy : EnemyOne
 {
+    Animator animator;
     public float attackTimer;
+
     void Update() {
+        //bool inRange = animator.GetBool(inRange);
+        animator = GetComponent<Animator>();
         Vector3 myLook = new Vector3(playerPos.position.x, gameObject.transform.position.y, playerPos.position.z);
         this.gameObject.transform.LookAt(myLook);
         attackTimer += Time.deltaTime;
@@ -14,10 +18,12 @@ public class AngerEnemy : EnemyOne
         float distance = Vector3.Distance(transform.position, playerPos.position);
         transform.LookAt(playerPos);
         if(distance > 3f) {
+            animator.SetBool("inRange", false);
             transform.position += transform.forward * speed * Time.deltaTime;
         }
         if(distance <= 3f) {
             if(attackTimer >= 1.5f) {
+                animator.SetBool("inRange", true);
                 GameManager.health -= 10;
                 attackTimer = 0f;
             }
